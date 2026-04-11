3502d4965b4518279daf6e01da55b6bffb149343
Dimas Bagas Saputro <dimazzbagazz@yahoo.co.id>
2026-04-11 12:39:07 +0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/1g1UC-tA)
+
+# 👁️ Overview
+
+Revofun is a web application for gaming company. In this website include information about our company, our products,
+and our simple game that can be play from our website.
+
+## 📃 Github Pages
+
+### Preview Web: [Click here!]()
+
+---
+
+## 📋 Features
+
+| Feature | Description |
+|---------|-------------|
+|
+
+---
+
+## 🛠️ Tech Stack
+
+[![My Skills](https://skillicons.dev/icons?i=js,html,css,tailwind)](https://skillicons.dev)
+
+- HTML: Used for structuring the content of the resume.
+- Tailwind CSS: Used for styling the resume and making it visually appealing.
+- JavaScript: Used for adding interactivity, such as click navbar.
+
+## 📸 Screenshots
+
+| Image | Description |
+|-------|-------------|
+|
+
+## 📂 Project Structure
+
+```bash
+milestone-2-Diba15-1/
+|-- assets/  # Assets Folder
+|   |-- images/ # Images Folder
+|   |-- css/ # CSS Folder
+|   |-- javascript/ # JS Folder
+|-- pages/ # Pages Folder
+|-- index.html
+|__ README.md
+```
---
+++ assets/javascript/index.js
@@ -0,0 +1,55 @@
+// Navbar Logic
+function activeNav(element) {
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (!mobileMenu.classList.contains('hidden')) {
+        toggleMobileMenu();
+    }
+
+    const allLinks = document.querySelectorAll('#navbar a, #mobile-menu a');
+    allLinks.forEach(link => link.classList.remove('active'));
+
+    element.classList.add('active');
+}
+
+function toggleMobileMenu() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+
+    if (!mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.add('fade');
+        mobileMenu.classList.remove('fadeOut');
+        document.body.style.overflow = 'hidden';
+    } else {
+        mobileMenu.classList.remove('fade');
+        mobileMenu.classList.add('fadeOut');
+        document.body.style.overflow = 'auto';
+    }
+}
+
+// Ketika scroll active class berubah
+window.addEventListener('scroll', () => {
+    let current = "";
+    const sections = document.querySelectorAll('section');
+    const scrollPos = window.scrollY || window.pageYOffset;
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+
+        if (scrollPos >= (sectionTop - 150)) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    const updateLinks = (selector) => {
+        document.querySelectorAll(selector).forEach(a => {
+            a.classList.remove('active');
+            const href = a.getAttribute('href');
+            if (href === `#${current}`) {
+                a.classList.add('active');
+            }
+        });
+    };
+
+    updateLinks('#navbar a');
+    updateLinks('#mobile-menu a');
+});
